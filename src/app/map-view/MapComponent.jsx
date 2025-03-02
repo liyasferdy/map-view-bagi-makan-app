4a322b9d0f39cbc723ef4b64c2a27611c285c3d9
liyasferdy <liyastarigan011@gmail.com>
2025-03-02 16:23:00 +0700

    add dummy loc

--- src/app/map-view/MapComponent.jsx
+++ src/app/map-view/MapComponent.jsx
@@ -4,11 +4,14 @@ import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
 function MapComponent() {
-  const position = [-7.782794123757193, 110.36709914609082];
+  const position1 = [-7.782794123757193, 110.36709914609082];
+  const position2 = [-7.803516642784995, 110.36461141110689];
+  const position3 = [-7.78231588155492, 110.3656809477647];
+  const position4 = [-7.78119742107567, 110.36765070796278];
 
   return (
     <MapContainer
-      center={position}
+      center={position1}
       zoom={17}
       scrollWheelZoom={false}
       style={{ height: "100vh", width: "100%" }}
@@ -19,8 +22,17 @@ function MapComponent() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>Halo dari Jakarta!</Popup>
+      <Marker position={position1}>
+        <Popup>Halo dari Tugu Jogja!</Popup>
+      </Marker>
+      <Marker position={position2}>
+        <Popup>Halo dari Rumah</Popup>
+      </Marker>
+      <Marker position={position3}>
+        <Popup>Pasar Kranggan</Popup>
+      </Marker>
+      <Marker position={position4}>
+        <Popup>Hotel</Popup>
       </Marker>
     </MapContainer>
   );
