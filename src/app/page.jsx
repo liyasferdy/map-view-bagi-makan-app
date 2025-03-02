01e31a5c97ced9a5999cdb1add139b3a7c6de985
liyasferdy <liyastarigan011@gmail.com>
2025-03-02 13:47:53 +0700

    add info

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+"use  client";
+
+// import { Button, ButtonGroup } from "@heroui/button";
+
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>bebek!</h1>
+      <h1>use routes: "/map-view" to preview the content </h1>
     </div>
   );
 }
